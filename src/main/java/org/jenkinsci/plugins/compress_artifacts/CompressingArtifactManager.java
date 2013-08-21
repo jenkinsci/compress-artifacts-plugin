ff16694ea69773fccfc4aff631f5e9ba6ddfdef9
Jesse Glick <jglick@cloudbees.com>
2013-08-21 16:38:45 -0400

    Updated.

--- src/main/java/org/jenkinsci/plugins/compress_artifacts/CompressingArtifactManager.java
+++ src/main/java/org/jenkinsci/plugins/compress_artifacts/CompressingArtifactManager.java
@@ -26,7 +26,6 @@ package org.jenkinsci.plugins.compress_artifacts;
 
 import hudson.FilePath;
 import hudson.Launcher;
-import hudson.Util;
 import hudson.model.BuildListener;
 import hudson.model.Run;
 import java.io.File;
@@ -36,14 +35,16 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Map;
-import java.util.TreeMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import jenkins.model.ArtifactManager;
+import jenkins.util.VirtualFile;
 
 final class CompressingArtifactManager extends ArtifactManager {
 
@@ -67,70 +68,167 @@ final class CompressingArtifactManager extends ArtifactManager {
         }
     }
 
-    @Override public boolean deleteArtifacts() throws IOException, InterruptedException {
+    @Override public boolean delete() throws IOException, InterruptedException {
         return archive().delete();
     }
 
-    @Override public Object browseArtifacts() {
-        throw new UnsupportedOperationException(); // TODO
+    @Override public VirtualFile root() {
+        return new VF("");
     }
 
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    @Override public Run.ArtifactList getArtifactsUpTo(int upTo) {
-        Map<String,Long> children = new TreeMap<String,Long>(String.CASE_INSENSITIVE_ORDER);
-        File archive = archive();
-        if (archive.isFile()) {
+    private final class VF extends VirtualFile {
+
+        private final String path;
+
+        VF(String path) {
+            this.path = path;
+        }
+
+        @Override public String getName() {
+            return path.replaceFirst("^(.+/)?([^/]+)/?$", "$2");
+        }
+
+        @Override public URI toURI() {
             try {
-                ZipFile zf = new ZipFile(archive);
-                try {
-                    Enumeration<? extends ZipEntry> entries = zf.entries();
-                    while (entries.hasMoreElements()) {
-                        ZipEntry entry = entries.nextElement();
-                        if (entry.isDirectory()) {
-                            continue;
-                        }
-                        children.put(entry.getName(), entry.getSize());
-                    }
-                } finally {
-                    zf.close();
-                }
-            } catch (IOException x) {
-                Logger.getLogger(CompressingArtifactManager.class.getName()).log(Level.WARNING, null, x);
+                return new URI(null, path, null);
+            } catch (URISyntaxException x) {
+                throw new AssertionError(x);
             }
         }
-        Run.ArtifactList r = build.new ArtifactList();
-        int n = 0;
-        for (Map.Entry<String,Long> e : children.entrySet()) {
-            String path = e.getKey();
-            // TODO build tree and collapse single items into parent node (cf. StandardArtifactManager; ArtifactList probably needs a helper API taking Enumeration<Map.Entry<String,Long>> files)
-            StringBuilder b = new StringBuilder();
-            for (String piece : path.split("/")) {
-                if (b.length() > 0) {
-                    b.append('/');
-                }
-                b.append(Util.rawEncode(piece));
-            }
-            r.add(build.new Artifact(path, path, b.toString(), Long.toString(e.getValue()), "n" + n));
-            if (++n >= upTo) {
-                break;
-            }
-        }
-        r.computeDisplayName();
-        return r;
-    }
 
-    @Override public InputStream loadArtifact(String artifact) throws IOException {
-        final ZipFile zf = new ZipFile(archive());
-        ZipEntry entry = zf.getEntry(artifact);
-        if (entry == null) {
-            zf.close();
-            throw new FileNotFoundException(artifact);
+        @Override public VirtualFile getParent() {
+            return new VF(path.replaceFirst("(/?[^/]+)/?$", "$1"));
         }
-        return new FilterInputStream(zf.getInputStream(entry)) {
-            @Override public void close() throws IOException {
+
+        private boolean looksLikeDir() {
+            return path.length() == 0 || path.endsWith("/");
+        }
+
+        @Override public boolean isDirectory() throws IOException {
+            if (!looksLikeDir()) {
+                return false;
+            }
+            ZipFile zf = new ZipFile(archive());
+            try {
+                Enumeration<? extends ZipEntry> entries = zf.entries();
+                while (entries.hasMoreElements()) {
+                    ZipEntry entry = entries.nextElement();
+                    String p = entry.getName();
+                    if (p.startsWith(path)) {
+                        return true;
+                    }
+                }
+                return false;
+            } finally {
                 zf.close();
             }
-        };
+        }
+
+        @Override public boolean isFile() throws IOException {
+            if (looksLikeDir()) {
+                return false;
+            }
+            ZipFile zf = new ZipFile(archive());
+            try {
+                return zf.getEntry(path) != null;
+            } finally {
+                zf.close();
+            }
+        }
+
+        @Override public boolean exists() throws IOException {
+            ZipFile zf = new ZipFile(archive());
+            try {
+                Enumeration<? extends ZipEntry> entries = zf.entries();
+                while (entries.hasMoreElements()) {
+                    ZipEntry entry = entries.nextElement();
+                    String p = entry.getName();
+                    if (looksLikeDir() ? p.startsWith(path) : p.equals(path)) {
+                        return true;
+                    }
+                }
+                return false;
+            } finally {
+                zf.close();
+            }
+        }
+
+        @Override public VirtualFile[] list() throws IOException {
+            if (!looksLikeDir()) {
+                throw new IOException();
+            }
+            ZipFile zf = new ZipFile(archive());
+            try {
+                Set<VirtualFile> files = new HashSet<VirtualFile>();
+                Enumeration<? extends ZipEntry> entries = zf.entries();
+                while (entries.hasMoreElements()) {
+                    ZipEntry entry = entries.nextElement();
+                    String p = entry.getName();
+                    if (p.startsWith(path)) {
+                        files.add(new VF(path + p.substring(path.length()).replaceFirst("/.+", "/")));
+                    }
+                }
+                return files.toArray(new VirtualFile[files.size()]);
+            } finally {
+                zf.close();
+            }
+        }
+
+        @Override public String[] list(String glob) throws IOException {
+            throw new IOException("TODO not implemented");
+        }
+
+        @Override public VirtualFile child(String name) {
+            // TODO this is ugly; would be better to not require / on path
+            VF f = new VF(path + name + '/');
+            try {
+                if (f.isDirectory()) {
+                    return f;
+                }
+            } catch (IOException x) {}
+            return new VF(path + name);
+        }
+
+        @Override public long length() throws IOException {
+            ZipFile zf = new ZipFile(archive());
+            try {
+                ZipEntry entry = zf.getEntry(path);
+                return entry != null ? entry.getSize() : 0;
+            } finally {
+                zf.close();
+            }
+        }
+
+        @Override public long lastModified() throws IOException {
+            ZipFile zf = new ZipFile(archive());
+            try {
+                ZipEntry entry = zf.getEntry(path);
+                return entry != null ? entry.getTime() : 0;
+            } finally {
+                zf.close();
+            }
+        }
+
+        @Override public boolean canRead() throws IOException {
+            return true;
+        }
+
+        @Override public InputStream open() throws IOException {
+            if (looksLikeDir()) {
+                throw new IOException();
+            }
+            final ZipFile zf = new ZipFile(archive());
+            ZipEntry entry = zf.getEntry(path);
+            if (entry == null) {
+                zf.close();
+                throw new FileNotFoundException(path);
+            }
+            return new FilterInputStream(zf.getInputStream(entry)) {
+                @Override public void close() throws IOException {
+                    zf.close();
+                }
+            };
+        }
     }
 
     private File archive() {
