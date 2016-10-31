61d3debf195b3094ddd0e0d0d6b1cc5d0ac51d24
Oliver Gondža <ogondza@gmail.com>
2016-10-31 12:08:44 +0100

    Add 4GB regression test

--- src/test/java/org/jenkinsci/plugins/compress_artifacts/CompressArtifactsTest.java
+++ src/test/java/org/jenkinsci/plugins/compress_artifacts/CompressArtifactsTest.java
@@ -25,20 +25,28 @@ package org.jenkinsci.plugins.compress_artifacts;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.iterableWithSize;
 import static org.junit.Assert.assertEquals;
 import hudson.FilePath;
+import hudson.Functions;
 import hudson.Launcher;
+import hudson.Util;
 import hudson.model.BuildListener;
+import hudson.model.Computer;
 import hudson.model.FreeStyleBuild;
 import hudson.model.AbstractBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Run;
+import hudson.remoting.Which;
 import hudson.tasks.ArtifactArchiver;
 import hudson.util.DescribableList;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ForkJoinPool;
 
 import jenkins.model.ArtifactManagerFactory;
 import jenkins.model.ArtifactManagerFactoryDescriptor;
@@ -46,16 +54,28 @@ import jenkins.model.ArtifactManagerConfiguration;
 import jenkins.model.Jenkins;
 
 import org.apache.commons.io.IOUtils;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.Bug;
+import org.jvnet.hudson.test.HudsonHomeLoader;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.TestBuilder;
+import org.jvnet.hudson.test.TestEnvironment;
 
 public class CompressArtifactsTest {
 
-    @Rule public JenkinsRule j = new JenkinsRule();
+    // We need to accumulate 4GB+ data and then archive it. That might not fit in everyone's /tmp partition so using ./target instead
+    @Rule public JenkinsRule j = new JenkinsRule().with(new HudsonHomeLoader() {
+        public File allocate() throws Exception {
+            File file = new File("target/CompressArtifactsTest-" + TestEnvironment.get().description().getMethodName());
+            if (file.exists()) {
+                Util.deleteRecursive(file);
+            }
+            return file;
+        }
+    });
 
     @Before
     public void setUp() {
@@ -127,6 +147,72 @@ public class CompressArtifactsTest {
                 artifactStream.close();
             }
         }
+    }
+
+    @Test @Bug(27042)
+    public void archiveLargerThan4GInTotal() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        final int artifactCount = 700;
+        p.getBuildersList().add(new TestBuilder() {
+            @Override public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+                final FilePath ws = build.getWorkspace();
+                final FilePath src = new FilePath(Which.jarFile(Jenkins.class));
+                for (int i = 0; i < artifactCount; i++) {
+                    final String name = "stuff" + i;
+//                    Computer.threadPoolForRemoting.submit(new Callable<Void>() {
+//                        public Void call() throws Exception {
+                            FilePath artifact = ws.child(name);
+                            artifact.copyFrom(src);
+//                            return null;
+//                        }
+//                    });
+                }
+                return true;
+            }
+        });
+        p.getPublishersList().add(new ArtifactArchiver("**/*", null, false));
+        FreeStyleBuild build = j.buildAndAssertSuccess(p);
+
+        final File archiveZip = new File(build.getRootDir(), "archive.zip");
+        long length = archiveZip.length();
+        assertThat(Functions.humanReadableByteSize(length), length, Matchers.greaterThanOrEqualTo(4L * 1024 * 1024 * 1024));
+
+        assertThat(build.getArtifacts(), Matchers.<Run<FreeStyleProject, FreeStyleBuild>.Artifact>iterableWithSize(artifactCount));
+
+
+//        CompressingArtifactManager.setup(jenkins);
+//
+//        FreeStyleJob job = jenkins.jobs.create();
+//        job.configure();
+//        job.addPublisher(ArtifactArchiver.class).includes("*");
+//        if (SystemUtils.IS_OS_UNIX) {
+//            job.addBuildStep(ShellBuildStep.class).command( // Generate archive larger than 4G
+//                    "#!/bin/bash\nwget $JENKINS_URL/jnlpJars/jenkins-cli.jar -O stuff.jar; for i in {0..7000}; do cp stuff.jar stuff.${i}.jar; done"
+//            );
+//        }
+//        else {
+//            // On windows although we could create hard links this would exceed the number of hard links you can create for a given file.
+//            // and sparse files compress too well.
+//            //job.addBuildStep(BatchCommandBuildStep.class).command("for /l %%x in (0, 1, 7000) do fsutil file createnew stuff.%%x.jar 819200");
+//            job.addBuildStep(BatchCommandBuildStep.class).command("powershell -command \"& { " +
+//                    "try { " +
+//                    "  Invoke-WebRequest %JENKINS_URL%/jnlpJars/jenkins-cli.jar -OutFile stuff.jar " +
+//                    "} catch { " +
+//                    "  echo 'download of jenkins-cli.jar failed'; " +
+//                    "  exit 2 "+
+//                    "}}\n " +
+//                    "for /l %%x in (0, 1, 7000) do cp stuff.jar stuff.%%x.jar\n");
+//
+//        }
+//        job.save();
+//
+//        Build build = job.scheduleBuild().waitUntilFinished(15 * 60).shouldSucceed();
+//
+//        long length = Long.parseLong(jenkins.runScript(
+//                "new FilePath(Jenkins.instance.getJob('%s').lastBuild.artifactsDir).parent.child('archive.zip').length()",
+//                job.name
+//        ));
+//        assertThat(length, greaterThanOrEqualTo(4L * 1024 * 1024 * 1024));
     }
 
     // Stolen from https://github.com/jenkinsci/jenkins/commit/cb5845db29bea10afd26c4425a44bc569ee75a7a
